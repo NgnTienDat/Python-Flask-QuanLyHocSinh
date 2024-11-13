89c3648ce042a06e5a47a0f2aa577885235e3516
Nguyen Tien Dat <tie.dat2004@gmail.com>
2024-11-13 23:51:48 +0700

    MOneToOne Done

--- QLHS-WebApp/qlhsapp/__init__.py
+++ QLHS-WebApp/qlhsapp/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from urllib.parse import quote
 
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tiendatmySQL964%40@localhost/qlhsdb?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@localhost/qlhsdb?charset=utf8mb4' % quote("tiendatmySQL964@")
 db = SQLAlchemy(app=app)
--- QLHS-WebApp/qlhsapp/models.py
+++ QLHS-WebApp/qlhsapp/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, Enum
-from qlhsapp import db
-from enum import Enum
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey, DateTime, Boolean, Table, column
+from sqlalchemy.orm import relationship
+from enum import Enum as UserEnum
+from qlhsapp import db, app
+from datetime import datetime
 
 # Models chứa các class là các table trong CSDL
 
@@ -9,7 +11,7 @@ class BaseModel(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
 
 
-class UserRole(Enum):
+class UserRole(UserEnum):
     ADMIN = 1
     STAFF = 2
     TEACHER = 3
@@ -21,77 +23,132 @@ class ScoreType(Enum):
     END_TERM = 3  # Cuoi ky
 
 
-# Tai khoan
-class Account(BaseModel):
-
-    pass
-
 class User(BaseModel):
-    __abstract__ = True
+    first_name = Column(String(20), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    phone_number = Column(String(12), nullable=False)
+    address = Column(String(255))
+    avatar = Column(String(255))
+
+    # OneToOne, uselist=False: Chi dinh moi quan he 1-1
+    account = relationship('Account', back_populates='user', uselist=False)
+    # staff = relationship('Staff', back_populates='user', uselist=False)
+    # teacher = relationship('Teacher', back_populates='user', uselist=False)
+    # administrator = relationship('Administrator', back_populates='user', uselist=False)
+
+
+
+# Tai khoan
+class Account(db.Model):
+    account_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    username = Column(String(100), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)
+    role = Column(Enum(UserRole), nullable=False)
+    created_date = Column(DateTime, default=datetime.now())
+    active = Column(Boolean, default=True)
+
+    # OneToOne voi User
+    user = relationship('User', back_populates='account')
+
 
 
 # Nhan vien
-class Staff(User):
-    pass
+class Staff(db.Model):
+    staff_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
-# Giao vien
-class Teacher(User):
-    pass
+    user = relationship('User', back_populates='staff')
+
 
 
 # Quan tri vien
-class Administrator(User):
-    pass
+class Administrator(db.Model):
+    admin_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
-
-# Quy dinh
-class Regulation(BaseModel):
-    pass
+    user = relationship('User', back_populates='administrator', uselist=False)
 
 
 
-class Student(BaseModel):
-    pass
+# Giao vien
+class Teacher(db.Model):
+
+    teacher_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    # OneToOne
+    user = relationship('User', back_populates='teacher', uselist=False)
+    # ManyToMany
+    subjects = relationship('Subject', secondary='teacher_subject', back_populates='teacher')
 
 
-# Khoi lop
-class GradeLevel(BaseModel):
-    pass
 
 
-
-class Class(BaseModel):
-    pass
-
-
-# Bang diem
-class ScoreBoard(BaseModel):
-    pass
-
-
-# Diem
-class Score(BaseModel):
-    pass
-
+# # Quy dinh
+# class Regulation(BaseModel):
+#     pass
+#
+#
+#
+# class Student(BaseModel):
+#     pass
+#
+#
+# # Khoi lop
+# class GradeLevel(BaseModel):
+#     pass
+#
+#
+#
+# class Class(BaseModel):
+#     pass
+#
+#
+# # Bang diem
+# class ScoreBoard(BaseModel):
+#     pass
+#
+#
+# # Diem
+# class Score(BaseModel):
+#     pass
+#
 
 # Mon hoc
 class Subject(BaseModel):
-    pass
-
-# Hoc Ky
-class Semester(BaseModel):
-    pass
+    name = Column(String(50), unique=True, nullable=False)
+    # ManyToMany
+    teachers = relationship('Teacher', secondary='teacher_subject', back_populates='subject')
 
 
-# Staff_Class, Many-To-Many
-class StaffClass(BaseModel):
-    pass
 
-# Student_Class, Many-To-Many
-class StudentClass(BaseModel):
-    pass
+#
+# # Hoc Ky
+# class Semester(BaseModel):
+#     pass
+#
+#
+# # Staff_Class, Many-To-Many
+# class StaffClass(BaseModel):
+#     pass
+#
+# # Student_Class, Many-To-Many
+# class StudentClass(BaseModel):
+#     pass
 
 # Teacher_Subject, Many-To-Many
-class TeacherSubject(BaseModel):
-    pass
+teacher_subject = Table('teacher_subject', db.Model.metadata,
+                        Column('id', Integer, primary_key=True, autoincrement=True),
+                        Column('teacher_id', Integer, ForeignKey('teacher.teacher_id')),
+                        Column('subject_id', Integer, ForeignKey('subject.id'))
+)
 
+
+
+if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
+        # student = Student(name='student 1')
+        # profile = StudentProfile(bio='HCMOU', student=student)
+        # profile2 = StudentProfile(bio='HCMOU 2', student=student)
+        # db.session.add(student)
+        # db.session.add(profile)
+        # db.session.add(profile)
+        # db.session.commit()
