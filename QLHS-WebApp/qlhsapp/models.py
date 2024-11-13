57a2c83c8238e7825c4c0df0ae91c69ad4942316
le trung <lequoctrunggg@gmail.com>
2024-11-13 22:49:37 +0700

    create table Student, Class, Grade, student_class

--- QLHS-WebApp/qlhsapp/models.py
+++ QLHS-WebApp/qlhsapp/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, Enum
-from qlhsapp import db
-from enum import Enum
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Date, Enum
+from qlhsapp import db, app
+from sqlalchemy.orm import relationship
+from enum import Enum as PyEnum
+from datetime import datetime
 
 
 # Models chứa các class là các table trong CSDL
@@ -10,14 +12,14 @@ class BaseModel(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
 
 
-class UserRole(Enum):
+class UserRole(PyEnum):
     ADMIN = 1
     STAFF = 2
     TEACHER = 3
 
 
 # Loai diem
-class ScoreType(Enum):
+class ScoreType(PyEnum):
     FIFTEEN = 1  # 15p
     FORTY_FIVE = 2  # 45p
     END_TERM = 3  # Cuoi ky
@@ -52,17 +54,37 @@ class Regulation(BaseModel):
     pass
 
 
-class Student(BaseModel):
-    pass
-
-
 # Khoi lop
 class GradeLevel(BaseModel):
-    pass
+    name = Column(String(50), nullable=False)
+    classes = relationship('Class', backref='gradelevel', lazy=True)
+
+
+student_class = db.Table('student_class',
+                         Column('student_id', Integer, ForeignKey('student.id'), primary_key=True),
+                         Column('class_id', Integer, ForeignKey('class.id'), primary_key=True)
+                         )
 
 
 class Class(BaseModel):
-    pass
+    name = Column(String(50), nullable=False)
+    # teacher_id
+    grade_id = Column(Integer, ForeignKey(GradeLevel.id), nullable=False)
+
+
+class GenderEnum(PyEnum):
+    MALE = "male"
+    FEMALE = "female"
+
+
+class Student(BaseModel):
+    name = Column(String(50), nullable=False)
+    address = Column(String(100))
+    email = Column(String(50))
+    gender = Column(Enum(GenderEnum, name="gender_enum"), nullable=False)
+    phone_number = Column(String(10))
+    date_of_birth = Column(Date, nullable=False)
+    classes = relationship('Class', secondary='student_class',  backref='students', lazy=True)
 
 
 # Bang diem
@@ -90,11 +112,12 @@ class StaffClass(BaseModel):
     pass
 
 
-# Student_Class, Many-To-Many
-class StudentClass(BaseModel):
-    pass
-
-
 # Teacher_Subject, Many-To-Many
 class TeacherSubject(BaseModel):
     pass
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
