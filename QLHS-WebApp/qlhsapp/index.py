2b992a87bf6250567c820613f4576f5e2b51aea0
le trung <lequoctrunggg@gmail.com>
2024-11-20 00:39:43 +0700

    update template

--- QLHS-WebApp/qlhsapp/index.py
+++ QLHS-WebApp/qlhsapp/index.py
@@ -21,10 +21,61 @@ def find_student_page():
     return render_template('admin/find-student.html')
 
 
-# Trung code: Tiep nhan hoc sinh
+# Tiếp nhận học sinh
 @app.route("/add-student")
 def add_student_page():
     return render_template('admin/add-student.html')
+
+
+# Phân lớp học sinh
+@app.route("/set-class")
+def set_class_page():
+    return render_template('admin/set-class.html')
+
+
+# Quy định số cột điểm
+@app.route("/score-regulation")
+def score_regulations_page():
+    return render_template('admin/score.html')
+
+
+# Quy định số học sinh
+@app.route("/numbers-regulation")
+def numbers_regulations_page():
+    return render_template('admin/numbers.html')
+
+
+# Quy định tuổi
+@app.route("/age-regulation")
+def age_regulations_page():
+    return render_template('admin/age.html')
+
+
+# Nhập điểm
+@app.route("/input-score")
+def input_score():
+    return render_template('admin/input-score.html')
+
+
+# Xuất điểm
+@app.route("/export-score")
+def export_score():
+    return render_template('admin/export-score.html')
+
+
+@app.route("/list-teacher")
+def list_teacher():
+    return render_template('admin/teacher.html')
+
+
+@app.route("/list-subject")
+def list_subject():
+    return render_template('admin/subject.html')
+
+
+@app.route("/list-class")
+def list_class():
+    return render_template('admin/class.html')
 
 
 if __name__ == '__main__':
