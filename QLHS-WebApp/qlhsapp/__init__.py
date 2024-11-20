004dd5fe37d65073b91984cd70e3e79d02301d24
Nguyen Tien Dat <tie.dat2004@gmail.com>
2024-11-21 00:16:49 +0700

    score column

--- QLHS-WebApp/qlhsapp/__init__.py
+++ QLHS-WebApp/qlhsapp/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import cloudinary
 
 app = Flask(__name__)
 
-
-# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1234@localhost/qlhsdb?charset=utf8mb4"
+app.secret_key = '^&*)%T*O&T*^&%)*^T%*&T)*O&RTO)(*#@$%@#$%*@FSK'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tiendatmySQL964%40@localhost/qlhsdb?charset=utf8mb4'
 
 
+
 db = SQLAlchemy(app=app)
+
+cloudinary.config(
+    cloud_name = 'dleseuevb',
+    api_key = '482437994953727',
+    api_secret = '6Xtl72oMfsJ6luFsm77ho6LnciU'
+)
