b2f13f64b92a9027db463035f8758e42fbaf9b7d
le trung <lequoctrunggg@gmail.com>
2024-11-22 08:08:20 +0700

    trung branch

--- QLHS-WebApp/qlhsapp/dao.py
+++ QLHS-WebApp/qlhsapp/dao.py
@@ -1,6 +1,6 @@
 # DAO chứa các phương thức tương tác xuống CSDL
 from qlhsapp import app, db
-from qlhsapp.models import ScoreRegulation, ScoreType, Score
+from qlhsapp.models import ScoreRegulation, ScoreType, Score, Student
 
 
 def load_score_regulation():
@@ -26,3 +26,23 @@ def add_score_regulation(score_type, score_quantity, coefficient):
 
 def get_score_type_by_name(name):
     return ScoreType.query.filter_by(name=name).first()
+
+
+def load_student(kw=None, page=1):
+    page_size = app.config['PAGE_SIZE']
+    start = (page - 1) * page_size
+
+    students = Student.query.offset(start).limit(page_size).all()
+
+    if kw:
+        students = [s for s in students if s.name.lower().find(kw.lower()) >= 0]
+
+    return students
+
+
+def get_student_by_id(student_id):
+    return Student.query.get(student_id)
+
+
+def count_student():
+    return Student.query.count()
