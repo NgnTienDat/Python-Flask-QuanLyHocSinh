2c46bfef6066f0dc9ca4f6e1b28f4c36abefe523
le trung <lequoctrunggg@gmail.com>
2024-11-23 08:15:49 +0700

    123

--- QLHS-WebApp/qlhsapp/__init__.py
+++ QLHS-WebApp/qlhsapp/__init__.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1234@localhost/qlhsdb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1234@localhost/qlhsdb?charset=utf8mb4"
 app.secret_key = '^&*)%T*O&T*^&%)*^T%*&T)*O&RTO)(*@#$%@#THIPQ#asf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tiendatmySQL964%40@localhost/qlhsdb?charset=utf8mb4'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tiendatmySQL964%40@localhost/qlhsdb?charset=utf8mb4'
 app.config['PAGE_SIZE'] = 8
 
 
